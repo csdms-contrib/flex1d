7c60ade68038804414115c0b2237a015e395d4ea
kettner <kettner@737c69e6-6460-0410-8e68-3bbafb0eb1e9>
2009-01-13 23:36:12 +0000

    Initial import

---
+++ flex1dandes.c
@@ -0,0 +1,155 @@
+#ifdef HAVE_MALLOC_H
+# include<malloc.h>
+#endif
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FREE_ARG char*
+#define NR_END 1
+
+#define PI 3.141592653589793
+
+float *vector(nl,nh)
+long nh,nl;
+/* allocate a float vector with subscript range v[nl..nh] */
+{
+        float *v;
+
+        v=(float *)malloc((unsigned int) ((nh-nl+1+NR_END)*sizeof(float)));
+        return v-nl+NR_END;
+}
+
+int *ivector(nl,nh)
+long nh,nl;
+/* allocate an int vector with subscript range v[nl..nh] */
+{
+        int *v;
+
+        v=(int *)malloc((unsigned int) ((nh-nl+1+NR_END)*sizeof(int)));
+        return v-nl+NR_END;
+}
+
+#define SWAP(a,b) tempr=(a);(a)=(b);(b)=tempr
+
+void four1(float data[], unsigned long nn, int isign)
+{
+	unsigned long n,mmax,m,j,istep,i;
+	double wtemp,wr,wpr,wpi,wi,theta;
+	float tempr,tempi;
+
+	n=nn << 1;
+	j=1;
+	for (i=1;i<n;i+=2) {
+		if (j > i) {
+			SWAP(data[j],data[i]);
+			SWAP(data[j+1],data[i+1]);
+		}
+		m=n >> 1;
+		while (m >= 2 && j > m) {
+			j -= m;
+			m >>= 1;
+		}
+		j += m;
+	}
+	mmax=2;
+	while (n > mmax) {
+		istep=mmax << 1;
+		theta=isign*(6.28318530717959/mmax);
+		wtemp=sin(0.5*theta);
+		wpr = -2.0*wtemp*wtemp;
+		wpi=sin(theta);
+		wr=1.0;
+		wi=0.0;
+		for (m=1;m<mmax;m+=2) {
+			for (i=m;i<=n;i+=istep) {
+				j=i+mmax;
+				tempr=wr*data[j]-wi*data[j+1];
+				tempi=wr*data[j+1]+wi*data[j];
+				data[j]=data[i]-tempr;
+				data[j+1]=data[i+1]-tempi;
+				data[i] += tempr;
+				data[i+1] += tempi;
+			}
+			wr=(wtemp=wr)*wpr-wi*wpi+wr;
+			wi=wi*wpr+wtemp*wpi+wi;
+		}
+		mmax=istep;
+	}
+}
+#undef SWAP
+
+void realft(float data[], unsigned long n, int isign)
+{
+	void four1(float data[], unsigned long nn, int isign);
+	unsigned long i,i1,i2,i3,i4,np3;
+	float c1=0.5,c2,h1r,h1i,h2r,h2i;
+	double wr,wi,wpr,wpi,wtemp,theta;
+
+	theta=3.141592653589793/(double) (n>>1);
+	if (isign == 1) {
+		c2 = -0.5;
+		four1(data,n>>1,1);
+	} else {
+		c2=0.5;
+		theta = -theta;
+	}
+	wtemp=sin(0.5*theta);
+	wpr = -2.0*wtemp*wtemp;
+	wpi=sin(theta);
+	wr=1.0+wpr;
+	wi=wpi;
+	np3=n+3;
+	for (i=2;i<=(n>>2);i++) {
+		i4=1+(i3=np3-(i2=1+(i1=i+i-1)));
+		h1r=c1*(data[i1]+data[i3]);
+		h1i=c1*(data[i2]-data[i4]);
+		h2r = -c2*(data[i2]+data[i4]);
+		h2i=c2*(data[i1]-data[i3]);
+		data[i1]=h1r+wr*h2r-wi*h2i;
+		data[i2]=h1i+wr*h2i+wi*h2r;
+		data[i3]=h1r-wr*h2r+wi*h2i;
+		data[i4] = -h1i+wr*h2i+wi*h2r;
+		wr=(wtemp=wr)*wpr-wi*wpi+wr;
+		wi=wi*wpr+wtemp*wpi+wi;
+	}
+	if (isign == 1) {
+		data[1] = (h1r=data[1])+data[2];
+		data[2] = h1r-data[2];
+	} else {
+		data[1]=c1*((h1r=data[1])+data[2]);
+		data[2]=c1*(h1r-data[2]);
+		four1(data,n>>1,-1);
+	}
+}
+
+main()
+{    float deltax,alpha,*w,dum,delrho,L,k;
+     int lattice_size_x,i; 
+     FILE *fp1,*fp2;   
+     
+     fp1=fopen("load1dandes","r");
+     fp2=fopen("load1ddeflect50","w");
+     lattice_size_x=4096;
+     deltax=0.52;
+     L=deltax*2*lattice_size_x;
+     alpha=50.0;  /* (D/(rho_c*g))^0.25 */
+     delrho=0.27; /* (rho_m-rho_c)/rho_m */
+     w=vector(1,2*lattice_size_x);
+     for (i=1;i<=2*lattice_size_x;i++) 
+      {if (i<lattice_size_x/2) fscanf(fp1,"%f %f",&w[i],&dum);
+	else w[i]=0;
+       if (fabs(w[i])<1000) w[i]=0.0;}
+     realft(w,2*lattice_size_x,1);
+     w[1]=w[1]/delrho;
+     k=PI/deltax;
+     w[2]*=1/(delrho+pow(k*alpha,4.0));
+     for (i=3;i<=2*lattice_size_x;i++) 
+      {k=((i-1)/2)*PI/L;
+       w[i]*=1/(delrho+pow(k*alpha,4.0));}
+     realft(w,2*lattice_size_x,-1);
+     for (i=1;i<=2*lattice_size_x;i++)
+      fprintf(fp2,"%f %f\n",i*deltax,w[i]/(lattice_size_x));
+     fclose(fp1);
+     fclose(fp2);
+}   
